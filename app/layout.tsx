d8924da567d4c4c9aa078bd3ecc009f121c2e1e4
lohargaurav00 <lohargaurav00@gmail.com>
2023-08-20 18:16:32 +0530

    layout setup , constructed reusable components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
 
-const font = Figtree({ subsets: ['latin'] })
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotify clone',
-  description: 'Listen to music for free!!',
-}
+  title: "Spotify clone",
+  description: "Listen to music for free!!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
-  )
+  );
 }
