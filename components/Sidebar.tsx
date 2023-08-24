cb918d25bb3b42e7ec83d9df62d21a7e0f605ce2
Gaurav Lohar <114788180+lohargaurav00@users.noreply.github.com>
2023-08-24 12:06:48 +0530

    song playing functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,10 +5,13 @@ import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 
+import { Song } from "@/types";
+
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
-import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+import { twMerge } from "tailwind-merge";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -17,6 +20,7 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   const pathname = usePathname();
+  const player = usePlayer();
 
   const routes = useMemo(
     () => [
@@ -36,9 +40,14 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
     [pathname]
   );
   return (
-    <div className="flex h-full">
-      <div 
-      className="
+    <div
+      className={twMerge(
+        "flex h-full",
+        player.activeId && "h-[calc(100%-80px)]"
+      )}
+    >
+      <div
+        className="
       hidden 
       md:flex 
       flex-col 
@@ -46,10 +55,11 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
       bg-black 
       h-full
       w-[300px] 
-      p-2">
+      p-2"
+      >
         <Box>
           <div
-          className="
+            className="
           flex
           flex-col
           gap-y-4
@@ -57,23 +67,19 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
           py-4"
           >
             {routes.map((item) => (
-              <SidebarItem 
-              key={item.label}
-              {...item}
-              />
+              <SidebarItem key={item.label} {...item} />
             ))}
           </div>
         </Box>
-        <Box 
+        <Box
           className="
           overflow-y-auto  
-          h-full">
-            <Library songs={songs}/>
+          h-full"
+        >
+          <Library songs={songs} />
         </Box>
       </div>
-      <main className="h-full flex-1 overflow-y-auto py-2">
-        {children}
-      </main>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
